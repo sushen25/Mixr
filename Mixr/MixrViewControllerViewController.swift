cefb8e511528925b9b64f001429ced0a9d935abd
sushen satturu <sushensatturu@sushens-MacBook-Pro.local>
2024-01-01 15:37:38 +1100

    Allow selecting of Ingredients

--- Mixr/MixrViewControllerViewController.swift
+++ Mixr/MixrViewControllerViewController.swift
@@ -8,17 +8,16 @@
 import UIKit
 
 
-class MixrViewController: UIViewController, CocktailApiDelegate, UITableViewDataSource {
+class MixrViewController: UIViewController, CocktailApiDelegate, UITableViewDataSource, UITableViewDelegate {
     let apiInterface = CocktailAPIInterface()
     
+    var allIngredients: [String:Bool] = [:]
+    var displayIngredients: [String:Bool] = [:]
+    
+    // MARK: - UI
     @IBOutlet weak var ingredientTableView: UITableView!
-    var allIngredients: [String] = []
-    var displayIngredients: [String] = []
+    @IBOutlet weak var selectedIngredients: UITextView!
     
-        
-    @IBOutlet weak var ingredientSearch: UITextField!
-    
-    // MARK: UI Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,7 +25,7 @@ class MixrViewController: UIViewController, CocktailApiDelegate, UITableViewData
         apiInterface.getAllIngredients()
         
         ingredientTableView.dataSource = self
-        
+        ingredientTableView.delegate = self
     }
 
     @IBAction func search(_ sender: Any) {
@@ -43,7 +42,7 @@ class MixrViewController: UIViewController, CocktailApiDelegate, UITableViewData
             displayIngredients = allIngredients
         } else {
             displayIngredients = allIngredients.filter { (element) -> Bool in
-                return element.lowercased().contains(searchText)
+                return element.key.lowercased().contains(searchText)
             }
         }
         
@@ -58,14 +57,44 @@ class MixrViewController: UIViewController, CocktailApiDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientCell", for: indexPath)
-        cell.textLabel?.text = displayIngredients[indexPath.row]
+        let ingredient = Array(displayIngredients.keys)[indexPath.row]
+        cell.textLabel?.text = ingredient
+        cell.accessoryType = (displayIngredients[ingredient]!) ? .checkmark : .none
+        
         return cell
     }
     
-    // MARK: API Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Deselect the row to visually show the selection
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        let ingredient = cell?.textLabel?.text
+        
+        allIngredients[ingredient!] = !allIngredients[ingredient!]!
+        displayIngredients[ingredient!] = !displayIngredients[ingredient!]!
+        
+        // Reload the selected row to update its appearance
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+        
+        updateSelectedIngredientsText()
+    }
+    
+    // MARK: - Methods
+    func updateSelectedIngredientsText() {
+        let selected = allIngredients.filter {$0.value == true}
+        let selectedList = selected.map { $0.key }
+        selectedIngredients.text = selectedList.joined(separator: ", ")
+    }
+    
+    
+    // MARK: - API Methods
     func didReceiveIngredientData(_ ingredients: [String]) {
-        allIngredients = ingredients
-        displayIngredients = ingredients
+        for ingredient in ingredients {
+            allIngredients[ingredient] = false
+        }
+        displayIngredients = allIngredients
+        
         DispatchQueue.main.async {
             self.ingredientTableView.reloadData()
         }
